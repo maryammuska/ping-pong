ed661df8b55e697ec08627c3bc63ee62415b3485
Maryam Muska <maryammuska@ucla.edu>
2023-01-10 12:39:41 -0800

    initial code

---
+++ pong.py
@@ -0,0 +1,12 @@
+import turtle 
+
+win = turtle.Screen()
+win.title("Pong by Maryam Muska")
+win.bgcolor("black")
+win.setup(width=800, height=600)
+win.tracer(0)
+
+#main game loop 
+while True:
+    win.update()
+    
