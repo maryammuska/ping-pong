2ee41cd70c9f43abda347ffe92813bc5a61abeaf
Maryam Muska <maryammuska@ucla.edu>
2023-01-19 22:55:30 -0800

    adding paddles and ball

--- pong.py
+++ pong.py
@@ -1,12 +1,48 @@
-import turtle 
+import os
+import turtle
 
-win = turtle.Screen()
-win.title("Pong by Maryam Muska")
-win.bgcolor("black")
-win.setup(width=800, height=600)
-win.tracer(0)
+wn = turtle.Screen()
+wn.title("Pong by Maryam Muska")
+wn.bgcolor("black")
+wn.setup(width=800, height=600)
+wn.tracer(0) #allows the screen to stop from updating
 
-#main game loop 
+#paddle A
+paddle_a = turtle.Turtle()
+paddle_a.speed(0)
+paddle_a.shape("square")
+paddle_a.color("white")
+paddle_a.shapesize(stretch_wid=5, stretch_len=1)
+paddle_a.penup()
+paddle_a.goto(-350, 0)
+
+#paddle B
+paddle_b = turtle.Turtle()
+paddle_b.speed(0)
+paddle_b.shape("square")
+paddle_b.color("white")
+paddle_b.shapesize(stretch_wid=5, stretch_len=1)
+paddle_b.penup()
+paddle_b.goto(350, 0)
+
+#Ball
+paddle_ball = turtle.Turtle()
+paddle_ball.speed(0)
+paddle_ball.shape("square")
+paddle_ball.color("white")
+paddle_ball.penup()
+paddle_ball.goto(0, 0)
+
+#functions
+def paddle_a_up():
+    y = paddle_a.ycor() #returning the y coordinate
+    y += 20 #add 20 pixels to y coordinate
+    paddle_a.sety(y)
+
+#Keyboard binding
+wn.listen() #this tells it to listen
+wn.onkeypress(paddle_a_up, "w") #when w is pressed, call the function to press paddle a up
+
+#main game loops
 while True:
-    win.update()
-    
+    wn.update()
